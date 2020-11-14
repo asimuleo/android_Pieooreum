e539c419b3ebd83611e301c34365142a122bd17c
Im Hyuk <cs.javah@kakao.com>
2020-11-14 15:44:46 +0900

    second commit

--- app/src/main/java/com/smu/team_andeu/MainActivity.java
+++ app/src/main/java/com/smu/team_andeu/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //g asfas da
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
