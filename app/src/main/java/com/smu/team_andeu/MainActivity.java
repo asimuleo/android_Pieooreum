e2781dde537b62298b6982473a03220cd8651836
fckd0 <fckd0908@gmail.com>
2020-11-14 15:49:56 +0900

    asdfasdg

--- app/src/main/java/com/smu/team_andeu/MainActivity.java
+++ app/src/main/java/com/smu/team_andeu/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //kjsda;lkgja;lksdjg
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
