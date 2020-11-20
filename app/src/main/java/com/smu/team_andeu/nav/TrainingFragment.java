8a1e8049f605f54f4d85fd172e89b3780b0030d5
Im Hyuk <cs.javah@kakao.com>
2020-11-20 23:26:46 +0900

    2020-11-20 23:12 : 승범님 코드 이전 완료

--- app/src/main/java/com/smu/team_andeu/nav/TrainingFragment.java
+++ app/src/main/java/com/smu/team_andeu/nav/TrainingFragment.java
@@ -6,6 +6,10 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,12 +17,21 @@ import androidx.fragment.app.Fragment;
 
 import com.smu.team_andeu.R;
 
-public class TrainingFragment extends Fragment {
+public class TrainingFragment extends Fragment implements AdapterView.OnItemSelectedListener {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         setHasOptionsMenu(true);
-        return inflater.inflate(R.layout.training_fragment, container, false);
+        View view = inflater.inflate(R.layout.training_fragment, container, false);
+
+        Spinner spinner = view.findViewById(R.id.spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(getContext(),
+                R.array.test, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+
+        return view;
     }
 
     @Override
@@ -34,5 +47,16 @@ public class TrainingFragment extends Fragment {
     @Override
     public void onDestroyView() {
         super.onDestroyView();
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String text = parent.getItemAtPosition(position).toString();
+        Toast.makeText(parent.getContext(), text, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
     }
 }
